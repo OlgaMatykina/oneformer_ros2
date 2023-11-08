63f1254893c45825d42648930fdc7f31c9bb0e68
OlgaMatykina <omatykina@mail.ru>
2023-11-08 21:17:37 +0300

    Changed build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -16,7 +16,7 @@ echo "Building for ${orange}${ARCH}${reset_color}"
 echo "Building is carried out in ${orange} ${NUM_THREADS} threads${reset_color}"
 
 docker build . \
-    -f $root_dir/Dockerfile \
+    -f $root_dir/Dockerfile_bridge \
     --build-arg UID=$(id -u) \
     --build-arg GID=$(id -g) \
     --build-arg NUM_THREADS=${NUM_THREADS} \
